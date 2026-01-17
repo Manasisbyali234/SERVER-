40fd5da309b229778ce528698dd2211ed3c033dc
Manasibyali124 <your_email_used_in_that_account@gmail.com>
2026-01-17 21:05:07 +0530

    Fix CORS policy for deployed client

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' ? process.env.CLIENT_URL : 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://client-460c.onrender.com'],
     credentials: true
 }));
 
